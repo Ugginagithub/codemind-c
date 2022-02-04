71acee49aeada39793c5239928d9553da8e00bbf
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-02-04 21:28:35 +0530

    #include<stdio.h> #include<math.h> int main() {     double s,a,b,c,area;     scanf("%lf%lf%lf",&a,&b,&c);     s=(a+b+c)/2;     area=(sqrt(s*(s-a)*(s-b)*(s-c)));     printf("%.2f",area);     return 0; }

---
+++ Area_of_Triangle_.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double s,a,b,c,area;
+    scanf("%lf%lf%lf",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=(sqrt(s*(s-a)*(s-b)*(s-c)));
+    printf("%.2f",area);
+    return 0;
+}
