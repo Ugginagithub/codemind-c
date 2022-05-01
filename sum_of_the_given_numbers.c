8e9ae643e689128e1bddb1fd8b57cf4590797b48
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-05-01 17:24:42 +0530

    #include<stdio.h> int main() {     int n,a,b,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&a,&b);         printf("%d ",a+b);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
+    int n,a,b,i;
     scanf("%d",&n);
-    int a[n],b[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d%d",&a[i],&b[i]);
-    }
-    for(i=0;i<n;i++)
-    {
+        scanf("%d%d",&a,&b);
         printf("%d
-",a[i]+b[i]);
+",a+b);
     }
 }
