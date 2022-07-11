2dc271ce65ae7957e716adf16c2159ee0cfbab8f
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-07-11 20:16:24 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || i==n+1-j)             {                 printf("%d ",n+1-i);             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -5,9 +5,16 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=i;j<=n;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d ",j);
+            if(i==j || i==n+1-j)
+            {
+                printf("%d ",n+1-i);
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
