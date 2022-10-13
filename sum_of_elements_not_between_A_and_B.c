dcf4cb35004bdce919d0eae2d6e9ffe597706141
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-10-13 21:49:11 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int x,y;     scanf("%d%d",&x,&y);     int max=a[0];     int c=0;     for(i=0;i<n;i++)     {         if(!(a[i]>=x&&a[i]<=y))         {             c=c+a[i];         }     }     printf("%d",c); }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int max=a[0];
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(!(a[i]>=x&&a[i]<=y))
+        {
+            c=c+a[i];
+        }
+    }
+    printf("%d",c);
+}
