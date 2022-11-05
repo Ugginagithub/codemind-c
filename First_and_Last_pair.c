8d46c831258f79c613fe69e238f709da499b253f
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-11-05 19:11:34 +0530

    #include<stdio.h> int main() {     int n,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n/2;i++)     {         printf("%d %d ",a[i],a[n-(i+1)]);     }     if(n%2!=0)     {         printf("%d 0",a[i]);     } }

---
+++ First_and_Last_pair.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        printf("%d %d ",a[i],a[n-(i+1)]);
+    }
+    if(n%2!=0)
+    {
+        printf("%d 0",a[i]);
+    }
+}
