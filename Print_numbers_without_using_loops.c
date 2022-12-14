f90561f4f7b3b40175a36fca317a791825ddf37d
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-12-14 10:25:31 +0530

    #include<stdio.h> int main() {     long n,i;     scanf("%ld",&n);     if(n>=1 && n<=10000)     {         i=0;         x:i++;         printf("%ld ",i);         if(i<n)         goto x;     }     else{         printf("The Number Series is Not Possible Print");     } }

---
+++ Print_numbers_without_using_loops.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    long n,i;
+    scanf("%ld",&n);
+    if(n>=1 && n<=10000)
+    {
+        i=0;
+        x:i++;
+        printf("%ld ",i);
+        if(i<n)
+        goto x;
+    }
+    else{
+        printf("The Number Series is Not Possible Print");
+    }
+}
